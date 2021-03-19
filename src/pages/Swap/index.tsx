be8391930885f7678d60990fbb3cf29dc73db2af
Paras Mani <parasmani300@gmail.com>
2021-03-19 13:07:46 +0530

    dhsjhds

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import ConnectWalletButton from 'components/ConnectWalletButton';
 
 export default function Swap() {
     return (
         <>
-            <h1>Hello there</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4 className="card-title">Buy Crypto to Your Wallet</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3"></div>
+                </div>
+            </div>
         </>
     )
 }
