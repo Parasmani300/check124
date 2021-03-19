c93f397b64281564e6d3246219864408c9b57a10
Paras Mani <parasmani300@gmail.com>
2021-03-19 14:05:51 +0530

    cjdchdjch

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -6,18 +6,16 @@ export default function Swap() {
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3"></div>
+                    <div className="col-md-3" />
                     <div className="col-md-6">
                         <div className="card">
                             <div className="card-body">
                                 <h4 className="card-title">Buy Crypto to Your Wallet</h4>
-                                <p>Hello there I am fine
-                                    
-                                </p>
+                                <p>Hello there I am fine</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3"></div>
+                    <div className="col-md-3" />
                 </div>
             </div>
         </>
