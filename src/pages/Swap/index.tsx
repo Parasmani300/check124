a4ec77a177b2b2a3ecd5cd3ff5e2a821ff45b354
Paras Mani <parasmani300@gmail.com>
2021-03-22 11:04:49 +0530

    cddsdhvsjhd

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -162,6 +162,7 @@ export default function Swap() {
               type="text"
               placeholder="Type a currency"
               className="c-Search__input"
+              onClick={()=> {setSecondOpen(!secondOpen)}} onKeyDown={() => {setSecondOpen(!secondOpen)}}
             />
             <select className="c-Search__search__dropdown">
               <option value="all" selected>ALL NETWORK</option>
